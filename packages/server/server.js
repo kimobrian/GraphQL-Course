d6891c797392d34bdf823166db8f3112eb1daeb7
kimobrian <brian.kimokoti@andela.com>
2018-02-01 22:34:55 +0300

    chore(final): Complete code for lesson 1

--- packages/server/server.js
+++ packages/server/server.js
@@ -37,12 +37,46 @@ type Person {
 }
 type Query {
   getAuthors: [Author]
+  retrieveAuthor(id: ID!): Author
+}
+type Mutation {
+  createAuthor(name: String!, gender: String!) : Author,
+  updateAuthor(id: ID!, name: String, gender: String, age: Int): Author
 }
 `;
 
 // The resolvers
 const resolvers = {
-  Query: { getAuthors: () => authors },
+  Query: { 
+    getAuthors: () => authors ,
+    retrieveAuthor: (obj, { id }) => authors.find(author => author.id === id)
+  },
+  Mutation: {
+    createAuthor: (obj, args) => {
+      const id = String(authors.length+1);
+      const { name, gender} = args;
+      const newAuthor = {
+        id,
+        info: {
+          name,
+          gender
+        }
+      }
+      authors.push(newAuthor);
+      return newAuthor;
+    },
+    updateAuthor: (obj, { id, name, gender, age}) => {
+      const author = authors.find(author => author.id === id);
+      if(author) {
+        if(name) author.name = name;
+        if(gender) author.gender = gender;
+        if(age) author.age = age;
+        return { id, info: author };
+      } else {
+        throw new Error('Author ID not found');
+      }
+    }
+  }
 };
 
 // Put together a schema
