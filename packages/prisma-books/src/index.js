5dcf18c70a896e67eb53b0fda4dbf56f5b167565
kimobrian <brian.kimokoti@andela.com>
2018-02-13 00:48:41 +0300

    feat(implement): Create and implement queries and mutations to fetch and create books

--- packages/prisma-books/src/index.js
+++ packages/prisma-books/src/index.js
@@ -1,58 +1,35 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { Prisma } = require('prisma-binding')
+const { GraphQLServer } = require("graphql-yoga");
+const { Prisma } = require("prisma-binding");
 
 const resolvers = {
   Query: {
-    feed(parent, args, ctx, info) {
-      return ctx.db.query.posts({ where: { isPublished: true } }, info)
-    },
-    drafts(parent, args, ctx, info) {
-      return ctx.db.query.posts({ where: { isPublished: false } }, info)
-    },
-    post(parent, { id }, ctx, info) {
-      return ctx.db.query.post({ where: { id } }, info)
-    },
+    fetchAllBooks: (parent, args, ctx, info) => {
+      return ctx.db.query.books({}, info);
+    }
   },
   Mutation: {
-    createDraft(parent, { title, text }, ctx, info) {
-      return ctx.db.mutation.createPost(
-        {
-          data: {
-            title,
-            text,
-            isPublished: false,
-          },
-        },
-        info,
-      )
-    },
-    deletePost(parent, { id }, ctx, info) {
-      return ctx.db.mutation.deletePost({ where: { id } }, info)
-    },
-    publish(parent, { id }, ctx, info) {
-      return ctx.db.mutation.updatePost(
-        {
-          where: { id },
-          data: { isPublished: true },
-        },
-        info,
-      )
-    },
-  },
-}
+    createBook: (parent, args, context, info) => {
+      const { title, description } = args;
+      return context.db.mutation.createBook(
+        { data: { title, description } },
+        info
+      );
+    }
+  }
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: req => ({
     ...req,
     db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://eu1.prisma.sh/brian-kim/prisma-books/dev', // the endpoint of the Prisma DB service
-      secret: 'mysecret123', // specified in database/prisma.yml
-      debug: true, // log all GraphQL queryies & mutations
-    }),
-  }),
-})
+      typeDefs: "src/generated/prisma.graphql",
+      endpoint: "https://eu1.prisma.sh/brian-kim/prisma-books/dev", // the endpoint of the Prisma DB service
+      secret: "mysecret123", // specified in database/prisma.yml
+      debug: true // log all GraphQL queryies & mutations
+    })
+  })
+});
 
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log("Server is running on http://localhost:4000"));
