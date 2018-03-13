287ceae5e9f4dddbd2399d0e62012c82acf292d5
kimobrian <brian.kimokoti@andela.com>
2018-03-13 17:27:18 +0300

    chore(books-component): Update BooksComponent to fetch list of books

--- packages/client/app/components/BooksComponent.jsx
+++ packages/client/app/components/BooksComponent.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {List, ListItem} from 'material-ui/List';
+import { List, ListItem } from 'material-ui/List';
+import CircularProgress from 'material-ui/CircularProgress';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default class ItemList extends Component {
-    constructor(props){
-        super(props);
-    }
+const getBooksQuery = gql`
+	query getAllBooks {
+		books: fetchAllBooks {
+            id
+			title
+			description
+		}
+	}
+`;
 
-    render() {
-        return <div> Books Component </div>
-    }
+class BooksComponent extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+        let { error, loading, books } = this.props.data;
+        if (error) return <div> An Error Occurred</div>
+        else if (loading) return <div style={ styles.loaderSection }><CircularProgress /></div> 
+		else return (
+			<Card expanded>
+    <CardHeader
+      title="Books List"
+    />
+    <CardText expandable={true}>
+      <List>
+          { books.map(book=> {
+              return (<ListItem key = { book.id } primaryText = { <p> {book.title} </p>} secondaryText = { <p> { book.description } </p> } secondaryTextLines = { 2 }/>)
+          })}
+      </List>
+    </CardText>
+  </Card>
+		);
+	}
 }
+
+const styles = {
+    loaderSection: {
+        textAlignt: "center"
+    }
+};
+
+const BooksComponentWithData = graphql(getBooksQuery)(BooksComponent);
+export default BooksComponentWithData;
