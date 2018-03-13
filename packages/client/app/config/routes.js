70c1b945dce811f08784afc3251b82e68e7db098
kimobrian <brian.kimokoti@andela.com>
2018-03-13 17:26:28 +0300

    chore(link-graphql): Create a GraphQL client in our React App

--- packages/client/app/config/routes.js
+++ packages/client/app/config/routes.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import BooksComponent from '../components/BooksComponent';
+import BooksComponentWithData from '../components/BooksComponent';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache()
+});
 
 const AppBarComponent = () => <AppBar title="GraphQL React" />;
 
 const routes = (
 	<MuiThemeProvider>
-		<BrowserRouter>
-			<div>
-				<AppBarComponent />
-				<Switch>
-					<Route exact path="/" component={BooksComponent} />
-				</Switch>
-			</div>
-		</BrowserRouter>
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<div>
+					<AppBarComponent />
+					<Switch>
+						<Route exact path="/" component={ BooksComponentWithData } />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</ApolloProvider>
 	</MuiThemeProvider>
 );
 
