4d3a3631a9f98c2728cdd726ad246de8d82977e1
kimobrian <brian.kimokoti@andela.com>
2018-03-16 23:03:41 +0300

    chore: Include routes for 'Create New Book' page and logout buttons

--- packages/client/app/config/routes.js
+++ packages/client/app/config/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import BooksComponentWithData from '../components/BooksComponent';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
@@ -11,12 +11,26 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
+import NewBook from '../components/NewBook';
+import FlatButton from 'material-ui/FlatButton';
+import { isLoggedIn, logout } from '../utils';
+
 const client = new ApolloClient({
 	link: new HttpLink({ uri: 'http://localhost:4000' }),
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
+	connectToDevTools: true
 });
 
-const AppBarComponent = () => <AppBar title="GraphQL React" />;
+let logoutButton = () => {
+	let button = isLoggedIn() ? (
+		<FlatButton label="LOGOUT" onClick={logout} />
+	) : window.location.pathname !== '/login' ? (
+		<FlatButton label="LOGIN" onClick={() => (window.location.href = '/login')} />
+	) : null;
+	return button;
+};
+
+const AppBarComponent = () => <AppBar title="GraphQL React" iconElementRight={logoutButton()} />;
 
 const routes = (
 	<MuiThemeProvider>
@@ -28,6 +42,13 @@ const routes = (
 					<Switch>
 						<Route exact path="/" component={BooksComponentWithData} />
 						<Route path="/login" component={Authentication} />
+						<Route
+							path="/new"
+							render={() => {
+								return isLoggedIn() ? <NewBook /> : <Redirect to="/login" />;
+							}}
+						/>
+						<Route path="*" component={Authentication} />
 					</Switch>
 				</div>
 			</BrowserRouter>
