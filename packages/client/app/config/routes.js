ed3c857a7a88c78b54960fe8ca120f01b2980efe
kimobrian <brian.kimokoti@andela.com>
2018-03-17 16:20:35 +0300

    chore(auth): Attach the auth token to requests

--- packages/client/app/config/routes.js
+++ packages/client/app/config/routes.js
@@ -11,49 +11,59 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
-import NewBook from '../components/NewBook';
+import NewBookComponentWithData from '../components/NewBook';
 import FlatButton from 'material-ui/FlatButton';
 import { isLoggedIn, logout } from '../utils';
+import { setContext } from 'apollo-link-context';
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authToken');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: Object.assign({}, headers, { Authorization: token ? `Bearer ${token}` : "" }) 
+  }
+});
 
 const client = new ApolloClient({
-	link: new HttpLink({ uri: 'http://localhost:4000' }),
-	cache: new InMemoryCache(),
-	connectToDevTools: true
+  link: authLink.concat(new HttpLink({ uri: 'http://localhost:4000' })),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
 });
 
 let logoutButton = () => {
-	let button = isLoggedIn() ? (
-		<FlatButton label="LOGOUT" onClick={logout} />
-	) : window.location.pathname !== '/login' ? (
-		<FlatButton label="LOGIN" onClick={() => (window.location.href = '/login')} />
-	) : null;
-	return button;
+  let button = isLoggedIn() ? (
+    <FlatButton label="LOGOUT" onClick={logout} />
+  ) : window.location.pathname !== '/login' ? (
+    <FlatButton label="LOGIN" onClick={() => (window.location.href = '/login')} />
+  ) : null;
+  return button;
 };
 
 const AppBarComponent = () => <AppBar title="GraphQL React" iconElementRight={logoutButton()} />;
 
 const routes = (
-	<MuiThemeProvider>
-		<ApolloProvider client={client}>
-			<BrowserRouter>
-				<div>
-					<AppBarComponent />
-					<br />
-					<Switch>
-						<Route exact path="/" component={BooksComponentWithData} />
-						<Route path="/login" component={Authentication} />
-						<Route
-							path="/new"
-							render={() => {
-								return isLoggedIn() ? <NewBook /> : <Redirect to="/login" />;
-							}}
-						/>
-						<Route path="*" component={Authentication} />
-					</Switch>
-				</div>
-			</BrowserRouter>
-		</ApolloProvider>
-	</MuiThemeProvider>
+  <MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div>
+          <AppBarComponent />
+          <br />
+          <Switch>
+            <Route exact path="/" component={BooksComponentWithData} />
+            <Route path="/login" component={Authentication} />
+            <Route
+              path="/new"
+              render={() => {
+                return isLoggedIn() ? <NewBookComponentWithData /> : <Redirect to="/login" />;
+              }}
+            />
+            <Route path="*" component={Authentication} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
+  </MuiThemeProvider>
 );
 
 export default routes;
